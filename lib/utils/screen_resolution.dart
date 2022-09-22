3aa343d409fee2b604b3687035ff665672656c29
blayziken <kingtoluwalope@gmail.com>
2022-09-22 15:41:46 +0100

    feature: verification
    #11 & #12

---
+++ lib/utils/screen_resolution.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+final yMargin2 = SizedBox(height: 2.h);
+final yMargin5 = SizedBox(height: 5.h);
+final yMargin10 = SizedBox(height: 10.h);
+final yMargin15 = SizedBox(height: 15.h);
+final yMargin20 = SizedBox(height: 20.h);
+final yMargin25 = SizedBox(height: 25.h);
+
+final yMargin30 = SizedBox(height: 30.h);
+final yMargin40 = SizedBox(height: 40.h);
+final yMargin50 = SizedBox(height: 50.h);
+final yMargin60 = SizedBox(height: 60.h);
+final yMargin70 = SizedBox(height: 70.h);
+final yMargin80 = SizedBox(height: 80.h);
+final yMargin90 = SizedBox(height: 90.h);
+
+final yMargin100 = SizedBox(height: 100.h);
+
+final xMargin2 = SizedBox(width: 2.h);
+final xMargin5 = SizedBox(width: 5.h);
+final xMargin10 = SizedBox(width: 10.h);
+final xMargin15 = SizedBox(width: 15.h);
+
+final xMargin20 = SizedBox(width: 20.h);
+final xMargin25 = SizedBox(width: 25.h);
+final xMargin30 = SizedBox(width: 30.h);
+final xMargin35 = SizedBox(width: 35.h);
+final xMargin40 = SizedBox(width: 40.h);
+final xMargin45 = SizedBox(width: 45.h);
+final xMargin50 = SizedBox(width: 50.h);
+final xMargin60 = SizedBox(width: 60.h);
+final xMargin70 = SizedBox(width: 70.h);
+
+// Widget customYMargin(double y) {
+//   return SizedBox(
+//     height: y,
+//   );
+// }
+
+// Widget customXMargin(double x) {
+//   return SizedBox(
+//     width: x,
+//   );
+// }
+
+// class MarginUtils {
+//   static final mg5 = SizedBox(height: 5.h);
+//   static final mg10 = SizedBox(height: 10.h);
+//   static final mg20 = SizedBox(height: 20.h);
+//   static final mg30 = SizedBox(height: 30.h);
+//   static final mg40 = SizedBox(height: 40.h);
+//   static final mg50 = SizedBox(height: 50.h);
+//   static final mg60 = SizedBox(height: 60.h);
+//   static final mg70 = SizedBox(height: 70.h);
+//   static final mg90 = SizedBox(height: 80.h);
+//   static final mg80 = SizedBox(height: 90.h);
+//   static final mg100 = SizedBox(height: 100.h);
+//   static final mg170 = SizedBox(height: 170.h);
+//   static final mg200 = SizedBox(height: 200.h);
+// }
+
+// class HMarginUtils {
+//   static final mg5 = SizedBox(width: 5.h);
+//   static final mg10 = SizedBox(width: 10.h);
+//   static final mg20 = SizedBox(width: 20.h);
+//   static final mg30 = SizedBox(width: 30.h);
+//   static final mg40 = SizedBox(width: 40.h);
+//   static final mg50 = SizedBox(width: 50.h);
+//   static final mg60 = SizedBox(width: 60.h);
+//   static final mg70 = SizedBox(width: 70.h);
+//   static final mg90 = SizedBox(height: 80.h);
+//   static final mg80 = SizedBox(height: 90.h);
+//   static final mg100 = SizedBox(height: 100.h);
+//   static final mg170 = SizedBox(height: 170.h);
+//   static final mg200 = SizedBox(width: 200.h);
+// }
