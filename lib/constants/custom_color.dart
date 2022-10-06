26cff4d48fd0c951fe458a9427fcc8bb3129f49a
blayziken <kingtoluwalope@gmail.com>
2022-10-06 16:56:11 +0100

    Remove const

--- lib/constants/custom_color.dart
+++ lib/constants/custom_color.dart
@@ -11,14 +11,14 @@ const onboardHeadingColor = Color(0xFF032E9A);
 const splashHeadingColor = Color(0xFF213656);
 const onboardTextColor = Color(0xFF213656);
 
-Color lightgreenshede = const Color(0xFFF0FAF6);
-Color lightgreenshede1 = const Color(0xFFB2D9CC);
-Color greenshede0 = const Color(0xFF66A690);
-Color greenshede1 = const Color(0xFF93C9B5);
-Color primarygreen = const Color(0xFF1E3A34);
-Color grayshade = const Color(0xFF93B3AA);
-Color colorAcent = const Color(0xFF78C2A7);
-Color cyanColor = const Color(0xFF6D7E6E);
+const Color lightgreenshede = Color(0xFFF0FAF6);
+const Color lightgreenshede1 = Color(0xFFB2D9CC);
+const Color greenshede0 = Color(0xFF66A690);
+const Color greenshede1 = Color(0xFF93C9B5);
+const Color primarygreen = Color(0xFF1E3A34);
+const Color grayshade = Color(0xFF93B3AA);
+const Color colorAcent = Color(0xFF78C2A7);
+const Color cyanColor = Color(0xFF6D7E6E);
 
 const kAnimationDuration = Duration(milliseconds: 200);
 
