a3465909145005c64c3f11d9b26a0043e9bcc7f5
sanjh.matelli@gmail.com <sanjh.matelli@gmail.com>
2022-10-14 16:56:26 +0530

    changed logo constant name

--- lib/constants/const.dart
+++ lib/constants/const.dart
@@ -1,6 +1,6 @@
 import 'package:grassroots_flutter/models/onboarding_model.dart';
 
-const logo = "assets/images/logo.png";
+const logo = "assets/images/logo2.png";
 const newLogo = "assets/images/logo2.png";
 const splashHeading = "Sarafu.Network";
 const walletImage = "assets/images/onboard4.png";
@@ -8,7 +8,16 @@ const walletHeading = "Set up your wallet";
 const walletDesc = "Import an existing wallet or create a new one";
 
 List<OnboardModel> allinonboardlist = [
-  OnboardModel("assets/images/onboard1.png", "Sarafu network prosper economies built by thriving communities", "Community Oriented"),
-  OnboardModel("assets/images/onboard2.png", "Explore the marketplace to exchange and support local businesses.", "Marketplace for everyone"),
-  OnboardModel("assets/images/onboard3.png", "Start by creating your own voucher and share it with your community.", "Marketplace for everyone"),
+  OnboardModel(
+      "assets/images/onboard1.png",
+      "Sarafu network prosper economies built by thriving communities",
+      "Community Oriented"),
+  OnboardModel(
+      "assets/images/onboard2.png",
+      "Explore the marketplace to exchange and support local businesses.",
+      "Marketplace for everyone"),
+  OnboardModel(
+      "assets/images/onboard3.png",
+      "Start by creating your own voucher and share it with your community.",
+      "Marketplace for everyone"),
 ];
