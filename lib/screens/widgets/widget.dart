b69b62825a5de929049a877fd12e325780ffb38f
blayziken <kingtoluwalope@gmail.com>
2022-10-20 23:11:01 +0100

    Design send voucher screen

--- lib/screens/widgets/widget.dart
+++ lib/screens/widgets/widget.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
-
-Widget circleIconWithText({required Widget icon, required String text}) {
-  return Column(
-    children: [
-      CircleAvatar(
-        radius: 25,
-        backgroundColor: Colors.blue[900],
-        child: icon,
-      ),
-      Text(
-        text,
-        style: const TextStyle(color: Colors.white),
-      )
-    ],
+Widget circleIconWithText({required Widget icon, required String text, required VoidCallback onTap}) {
+  return GestureDetector(
+    onTap: onTap,
+    child: Column(
+      children: [
+        CircleAvatar(
+          radius: 25,
+          backgroundColor: Colors.blue[900],
+          child: icon,
+        ),
+        Text(
+          text,
+          style: const TextStyle(color: Colors.white),
+        )
+      ],
+    ),
   );
 }
