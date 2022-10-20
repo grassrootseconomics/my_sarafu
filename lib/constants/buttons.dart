66a5aae9083d0faf0f93b4fdf9900dbcd7fa55c0
blayziken <kingtoluwalope@gmail.com>
2022-10-20 22:50:50 +0100

    Create custom button component

---
+++ lib/constants/buttons.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+enum ButtonState { idle, loading, disabled }
+
+class SarafuButton extends StatefulWidget {
+  final String title;
+  final IconData? leadingIcon, endIcon;
+  final Function()? onTap;
+  final Color? textColor, color;
+  final double? width, height;
+  final double? textSize;
+  final FontWeight? textWeight;
+  final FontStyle? fontStyle;
+
+  const SarafuButton({
+    Key? key,
+    required this.title,
+    this.leadingIcon,
+    required this.onTap,
+    this.textColor,
+    this.width,
+    this.textSize,
+    this.height,
+    this.color,
+    this.endIcon,
+    this.textWeight,
+    this.fontStyle,
+  }) : super(key: key);
+
+  @override
+  State<SarafuButton> createState() => _SarafuButtonState();
+}
+
+class _SarafuButtonState extends State<SarafuButton> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: widget.onTap,
+      child: Container(
+        height: widget.height ?? 50.h,
+        width: widget.width ?? 130.w,
+        decoration: BoxDecoration(
+          color: widget.color ?? const Color(0xFF032E9A),
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (widget.leadingIcon != null)
+              Padding(
+                padding: const EdgeInsets.only(right: 5),
+                child: Icon(
+                  widget.leadingIcon,
+                  color: Colors.white,
+                  size: 16.sp,
+                ),
+              ),
+            Text(
+              widget.title,
+              style: TextStyle(
+                fontSize: widget.textSize ?? 16.sp,
+                color: widget.textColor,
+                fontWeight: widget.textWeight,
+                fontStyle: widget.fontStyle,
+              ),
+            ),
+            if (widget.endIcon != null)
+              Padding(
+                padding: const EdgeInsets.only(left: 5),
+                child: Icon(
+                  widget.endIcon,
+                  color: Colors.white,
+                  size: 16.sp,
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
