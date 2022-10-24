01234223c535cedad9181731fa9f81b67016e415
kranthi <lci2021014@iiitl.ac.in>
2022-10-24 12:49:32 +0530

    #68

---
+++ lib/base_widgets/custom_full_width_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Customfullwidthbutton extends StatefulWidget {
+  const Customfullwidthbutton({super.key});
+
+  @override
+  State<Customfullwidthbutton> createState() => _CustomfullwidthbuttonState();
+}
+
+class _CustomfullwidthbuttonState extends State<Customfullwidthbutton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
